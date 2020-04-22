1a99d8b60a3f4233bf230715632d2900bf264317
Elden Li <lielden@yahoo.com>
2020-04-21 21:53:05 -0700

    added crud

--- app.py
+++ app.py
@@ -31,9 +31,9 @@ cred = credentials.Certificate('key.json')
 default_app = initialize_app(cred)
 db = firestore.client() 
 
-# given collection, and document, adds an entry into db
-@app.route('/add', methods=['GET', 'POST'])
-def create():
+# given collection, and document, adds an name/age entry into db
+@app.route('/add', methods=['POST'])
+def create_employee():
     try:
         body = request.json
         id = body['id']
@@ -51,56 +51,66 @@ def create():
     except Exception as e:
         return f"An Error Occured: {e}"
 
-# @app.route('/list', methods=['GET'])
-# def read():
-#     """
-#         read() : Fetches documents from Firestore collection as JSON
-#         todo : Return document that matches query ID
-#         all_todos : Return all documents
-#     """
-#     try:
-#         # Check if ID was passed to URL query
-#         todo_id = request.args.get('id')    
-#         if todo_id:
-#             todo = todo_ref.document(todo_id).get()
-#             return jsonify(todo.to_dict()), 200
-#         else:
-#             all_todos = [doc.to_dict() for doc in todo_ref.stream()]
-#             return jsonify(all_todos), 200
-#     except Exception as e:
-#         return f"An Error Occured: {e}"
+# returns the requested 
+# json req keys: id, collection, document, field
+@app.route('/list', methods=['GET'])
+def read_employee_info():
+    try:
+        body = request.json
+        id = body['id']
+        collection = body['collection']
+        document = body['document']
+        doc_ref = db.collection(collection).document(document+str(id))
+        doc = doc_ref.get()
 
-# @app.route('/update', methods=['POST', 'PUT'])
-# def update():
-#     """
-#         update() : Update document in Firestore collection with request body
-#         Ensure you pass a custom ID as part of json body in post request
-#         e.g. json={'id': '1', 'title': 'Write a blog post today'}
-#     """
-#     try:
-#         id = request.json['id']
-#         todo_ref.document(id).update(request.json)
-#         return jsonify({"success": True}), 200
-#     except Exception as e:
-#         return f"An Error Occured: {e}"
+        if doc.exists:
+            doc = doc.to_dict()
+            return jsonify({
+                'name': doc['name'],
+                'age': doc['age']
+            })
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+# updates an employee field
+# json req requires: id, collection, document, field, update
+@app.route('/update', methods=['POST', 'PUT'])
+def update():
+    """
+        update() : Update document in Firestore collection with request body
+        Ensure you pass a custom ID as part of json body in post request
+        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    """
+    try:
+        body = request.json
+        id = body['id']
+        collection = body['collection']
+        document = body['document']
+        doc_ref = db.collection(collection).document(document+str(id))
+        doc_ref.update({body['field']: body['update']})
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
 
-# @app.route('/delete', methods=['GET', 'DELETE'])
-# def delete():
-#     """
-#         delete() : Delete a document from Firestore collection
-#     """
-#     try:
-#         # Check for ID in URL query
-#         todo_id = request.args.get('id')
-#         todo_ref.document(todo_id).delete()
-#         return jsonify({"success": True}), 200
-#     except Exception as e:
-#         return f"An Error Occured: {e}"
+# delete an employee
+# json requires: id
+@app.route('/delete', methods=['GET'])
+def delete_employee():
+    """
+        delete() : Delete a document from Firestore collection
+    """
+    try:
+        # Check for ID in URL query
+        body = request.json
+        id = body['id']
+        doc_ref = db.collection('employees').document('employee'+str(id))
+        doc_ref.delete()
+        
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
 
-# The route() function of the Flask class is a decorator,  
-# which tells the application which URL should call  
-# the associated function. 
 @app.route('/') 
 # ‘/’ URL is bound with hello_world() function. 
 def hello_world(): 
